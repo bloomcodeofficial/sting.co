640a0ae3620182fa418390dd6cdf9a383d64772a
niklashans <104555311+niklashans@users.noreply.github.com>
2023-05-04 11:28:27 +0200

    1.0.6
    - Added accordion functionalities for /pre-sting page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { logoMarquee } from '$utils/logo-marquee';
 import { navbar } from '$utils/navbar';
+import { prestingAcc } from '$utils/presting-acc';
 import { senja } from '$utils/senja';
 navbar();
 senja();
 logoMarquee();
+prestingAcc();
