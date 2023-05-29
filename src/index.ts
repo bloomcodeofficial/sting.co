6a0f7e769c1e94543ed534abeff964ebfff351f4
niklashans <104555311+niklashans@users.noreply.github.com>
2023-05-29 11:15:11 +0200

    Updates to Jobylon integration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { faq1 } from '$utils/faq-1';
 import { filter } from '$utils/filter';
-import { jobylon } from '$utils/jobylon';
 import { library } from '$utils/library';
 import { navbar } from '$utils/navbar';
 import { prestingAcc } from '$utils/presting-acc';
@@ -16,4 +15,3 @@ videoJS();
 faq1();
 filter();
 library();
-jobylon();
